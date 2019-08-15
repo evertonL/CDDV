f1353a40116dda9c9bb39c72552e879a4eb258ba
evertonL <evertonluizestiva@gmail.com>
2019-08-15 17:34:47 -0300

    atualizando

--- projetoTCC/src/app/cadastrar-ads/cadastrar-ads.component.ts
+++ projetoTCC/src/app/cadastrar-ads/cadastrar-ads.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarAdsComponent implements OnInit {
 
-  constructor() { }
+  private inputCpfAgente: number;
+  private inputNameAgente: string;
+  private inputSenhaAgente: number; //preciso tratar que so posso adicionar a senha ao banco quando senha e confirmar senhar for igual
+  private inputConfirmaSenhaAgente: number;
+  private inputRgAgente: number;
+
+  
+    /**
+     * @constructor
+     * @param {number } inputCpfAgente  
+     * @param {string } inputNameAgente 
+     * @param {number } inputSenhaAgente
+     * @param {number } inputConfirmaSenhaAgente
+     * @param {number } inputRgAgente
+     */
+  constructor(inputCpfAgente?: number, inputNameAgente?: string, inputSenhaAgente?: number,
+    inputConfirmaSenhaAgente?: number, inputRgAgente?: number) {
+
+    this.inputCpfAgente = inputCpfAgente;
+    this.inputNameAgente = inputNameAgente;
+    this.inputSenhaAgente = inputSenhaAgente;
+    this.inputConfirmaSenhaAgente = inputConfirmaSenhaAgente;
+    this.inputRgAgente = inputRgAgente;
+  }
 
   ngOnInit() {
   }
